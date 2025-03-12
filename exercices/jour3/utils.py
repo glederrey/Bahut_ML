f3a4cd3764e4a0da53ed85778c9bdf5d132acd28
glederrey <glederre@gmail.com>
2025-03-12 22:32:59 +0100

    [UPD] use smaller version of dataset

--- exercices/jour3/utils.py
+++ exercices/jour3/utils.py
@@ -7,12 +7,20 @@ from sklearn.metrics import mean_absolute_error as mae
 
 def results_predictions_satisfaction(y_pred):
 
-    path = '../../data/passenger_satisfaction/y_test.pkl'
-    with open(path, 'rb') as f:
-        y_test = pickle.load(f)
+    names = ['y_test_20.pkl', 'y_test_50.pkl', 'y_test.pkl']
 
-    if len(y_pred) != len(y_test):
-        raise ValueError(f"y_pred and y_test must have the same length ({len(y_pred)} != {len(y_test)})")
+    found_corresponding_file = False
+    for n in names:
+        path = f'../../data/passenger_satisfaction/{n}'
+        with open(path, 'rb') as f:
+            y_test = pickle.load(f)
+
+        if len(y_test) == len(y_pred):
+            found_corresponding_file = True
+            break
+
+    if not found_corresponding_file:
+        raise ValueError("Aucun fichier y_test correspondant à y_pred trouvé !")   
 
     # Check if there are other stuff than 'satisfied' or 'neutral or dissatisfied' in y_pred
     if not all(pred in ['satisfied', 'neutral or dissatisfied'] for pred in y_pred):
@@ -25,23 +33,25 @@ def results_predictions_satisfaction(y_pred):
 
     f1_score_val = f1_score(y_test, y_pred)
 
-    print(f"F1 score is: {f1_score_val:.5f}")
-
     return f1_score_val
 
 def results_predictions_prices(y_pred):
 
-    path = '../../data/flight_prices/y_test.pkl'
-    with open(path, 'rb') as f:
-        y_test = pickle.load(f)
+    names = ['y_test_20.pkl', 'y_test_50.pkl', 'y_test.pkl']
 
-    if len(y_pred) != len(y_test):
-        raise ValueError(f"y_pred and y_test must have the same length ({len(y_pred)} != {len(y_test)})")
+    found_corresponding_file = False
+    for n in names:
+        path = f'../../data/flight_prices/{n}'
+        with open(path, 'rb') as f:
+            y_test = pickle.load(f)
+
+        if len(y_test) == len(y_pred):
+            found_corresponding_file = True
+            break
+
+    if not found_corresponding_file:
+        raise ValueError("Aucun fichier y_test correspondant à y_pred trouvé !")   
 
     mae_val = mae(y_test, y_pred)
 
-    print(f"MAE is: {mae_val:.5f}")
-
     return mae_val
-
-
